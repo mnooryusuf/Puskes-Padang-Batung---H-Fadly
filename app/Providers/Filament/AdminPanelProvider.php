09830928ac561245ce06bb1de150028e917d0f49
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2026-03-08 11:00:13 +0800

    Fitur: Mengatur nama merek untuk panel admin Filament.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,6 +25,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->brandName('Aplikasi Rawat Jalan Puskesmas')
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
