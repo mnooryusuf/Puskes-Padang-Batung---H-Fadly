6dcae7bd78697d0de341739f1f4ae548fe3c93f5
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2026-03-09 18:13:41 +0800

    prestasi: Tambahkan grup navigasi 'Pelayanan', 'Data Master', dan 'Manajemen' ke panel admin Filament.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,11 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => '#00796B',
                 'warning' => '#FF9800',
             ])
+            ->navigationGroups([
+                'Pelayanan',
+                'Data Master',
+                'Manajemen',
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
